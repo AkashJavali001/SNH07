95acfb0e74d4248a42625de067d861dd4c6fea2d
Unknownhunt <hunterzanonymous@gmail.com>
2023-11-26 10:32:12 +0530

    init commit

---
+++ Frontend/src/pages/About/index.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
+import "./About.css";
+import aboutImg from '../../assets/images/aboutSipeka.svg'
+import '../../shared/Shared.css'
+import { BottomLine } from "../../components/atoms";
+import { Footer, Navbar } from "../../components";
+
+const About = () => {
+    const location = useLocation();
+    const isHomePage = location.pathname === "/";
+
+    return (
+        <>
+            <Navbar />
+            <div className="dark:bg-boxdark">
+                <div className="parent pt-16 my-16">
+                    <div>
+                        <motion.div
+                            className="mb-10"
+                            initial={{ y: -200, opacity: 0 }}
+                            animate={{
+                                y: 0,
+                                opacity: 1,
+                                transition: { duration: 1, type: "spring" },
+                            }}
+                        >
+                            <h3 className="text-neutral text-center dark:text-white">About</h3>
+                            <h1 className="text-4xl font-semibold drop-shadow-md text-center text-accent dark:text-white">
+                                WORK<span className="text-primary">EASE</span>
+                            </h1>
+                            <BottomLine />
+                        </motion.div>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-20">
+                            <motion.div
+                                initial={{ x: -200, opacity: 0 }}
+                                animate={{
+                                    x: 0,
+                                    opacity: 1,
+                                    transition: { duration: 1, delay: 1.25 },
+                                }}
+                            >
+                                <img
+                                    src={aboutImg}
+                                    alt="Key Features"
+                                    className="w-100 h-100 transform translate-y-[-12%]"
+                                    title="Key Features"
+                                />
+
+                            </motion.div>
+                            <motion.div
+                                initial={{ x: 200, opacity: 0 }}
+                                animate={{
+                                    x: 0,
+                                    opacity: 1,
+                                    transition: { duration: 1, delay: 1.25 },
+                                }}
+                            >
+                                <p className="font-medium text-center translate-y-[-60%] sm:translate-y-[-0%] sm:mb-2 md:text-left dark:text-white">
+                                    Streamlined Onboarding:Accelerate the Onboarding process for new hires with our digital onboarding system.Say goodbye to paperwork and hello to a seamless integration experience.<br />
+                                    Innovative Employee Profiles:Foster a sense of communitywith visually appealling and interactive employee profiles. Highlight skills, achivements, and personal interests.
+                                </p>
+                                <br />
+                                <p className="font-medium text-center translate-y-[-50%] sm:translate-y-[-0%] sm:mb-2 md:text-left dark:text-white">
+                                    Automated Salary Calculations:Take the Hassel out of payroll managment. Our automated system ensures accuracte and transperent salary calculations.<br />
+                                    Advanced Efficincy Tracking:Monitor Performance with our advanced tracking system. Set goals, track progress, and provide real-time feedbackto enhance employee productivity.
+                                    
+                                </p>
+                            </motion.div>
+                        </div>
+                    </div>
+                </div>
+                {!isHomePage && <Footer />}
+            </div>
+        </>
+    );
+};
+
+export default About;
